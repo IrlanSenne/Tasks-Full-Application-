6375b27c31031be622b200f9ccbd91e8533f127b
Moreira <im.senne2017@gmail.com>
2021-02-24 14:49:55 +0000

    Retrofit Call implemented

--- app/src/main/java/com/example/tasks/service/repository/remote/PersonRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/remote/PersonRepository.kt
@@ -1,10 +1,27 @@
 package com.example.tasks.service.repository.remote
 
+import com.example.tasks.service.HeaderModel
+import com.example.tasks.service.listener.ApiListener
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 class PersonRepository {
 
-    val remote = RetrofitClient.createService(PersonService::class.java)
+    private val mRemote = RetrofitClient.createService(PersonService::class.java)
 
-    fun login(email: String, password: String) {
-        //  remote.login(email, password)
+    fun login(email: String, password: String, listener: ApiListener) {
+        val call : Call<HeaderModel> = mRemote.login(email, password)
+        //Async
+        call.enqueue(object : Callback<HeaderModel>{
+            override fun onFailure(call: Call<HeaderModel>, t: Throwable) {
+               listener.onFalirure(t.message.toString())
+            }
+
+            override fun onResponse(call: Call<HeaderModel>, response: Response<HeaderModel>) {
+                response.body()?.let { listener.onSuccess(it) }
+            }
+
+        })
     }
 }
--- app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -2,13 +2,28 @@ package com.example.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.example.tasks.service.HeaderModel
+import com.example.tasks.service.listener.ApiListener
+import com.example.tasks.service.repository.remote.PersonRepository
 
-class LoginViewModel(application: Application) : AndroidViewModel(application) {
+class LoginViewModel(application: Application) : AndroidViewModel(application){
+
+private val mRersonRepository = PersonRepository()
 
     /**
      * Faz login usando API
      */
     fun doLogin(email: String, password: String) {
+        mRersonRepository.login(email, password, object: ApiListener{
+            override fun onSuccess(model: HeaderModel) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFalirure(str: String) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 
     /**
