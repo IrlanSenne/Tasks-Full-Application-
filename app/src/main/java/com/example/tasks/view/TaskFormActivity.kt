32aa48b2da6e54f16e3dc631fc685d49a56ce829
Moreira <im.senne2017@gmail.com>
2021-02-25 18:56:30 +0000

    Date Picker and its methods

--- app/src/main/java/com/example/tasks/view/TaskFormActivity.kt
+++ app/src/main/java/com/example/tasks/view/TaskFormActivity.kt
@@ -1,16 +1,26 @@
 package com.example.tasks.view
 
+import android.app.DatePickerDialog
+import android.icu.text.SimpleDateFormat
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModelProvider
 import com.example.tasks.R
 import com.example.tasks.viewmodel.RegisterViewModel
 import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_register.button_save
+import kotlinx.android.synthetic.main.activity_task_form.*
+import java.util.*
 
-class TaskFormActivity : AppCompatActivity(), View.OnClickListener {
+class TaskFormActivity : AppCompatActivity(), View.OnClickListener, DatePickerDialog.OnDateSetListener {
 
     private lateinit var mViewModel: RegisterViewModel
+
+    private val mDateFormat = java.text.SimpleDateFormat("dd,MM,yyyy", Locale.ENGLISH)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +42,17 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener {
             val password = edit_password.text.toString()
 
             mViewModel.create(name, email, password)
+        } else if (id == R.id.button_date) {
+                showDatePicker()
         }
+    }
+
+    private fun showDatePicker() {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this, this, year, month, day).show()
     }
 
     private fun observe() {
@@ -40,6 +60,17 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun listeners() {
         button_save.setOnClickListener(this)
+        button_date.setOnClickListener(this)
     }
+
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+       val calendar = Calendar.getInstance()
+        calendar.set(year, month, dayOfMonth)
+
+        val str = mDateFormat.format(calendar.time)
+        button_date.text = str
+    }
+
 
 }
